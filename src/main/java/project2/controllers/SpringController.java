81234396d4b0c9defc65a0162de765060a51c300
Amira Koukach <amirakoukach5066@gmail.com>
2023-12-16 12:16:40 +0200

    Added a static template.

--- src/main/java/project2/controllers/SpringController.java
+++ src/main/java/project2/controllers/SpringController.java
@@ -4,43 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("spring")
 public class SpringController {
-    // lives /spring/helloSpring
-    @GetMapping("helloSpring")
-    public String hello() {
-        return "Hello Spring!";
-    }
-
-    // lives /spring/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye Spring!";
-    }
-
     // Handles request of the form /hello?name=LaunchCode
-    // Accepts both GET and POST requests.
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello " + name + "!";
     }
 
     // Handles request of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + // submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 }
