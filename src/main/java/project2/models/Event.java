9861c8568517d415954221d5cef682b29065d339
Amira Koukach <amirakoukach5066@gmail.com>
2023-12-16 16:15:57 +0200

    Added Event model.

---
+++ src/main/java/project2/models/Event.java
@@ -0,0 +1,32 @@
+package project2.models;
+
+public class Event {
+    private String name;
+    private String description;
+
+    public Event(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
