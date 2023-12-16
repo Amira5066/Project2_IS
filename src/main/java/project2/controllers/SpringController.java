eb5be0df048ee3336fc4d14e52d9ce160565be02
Amira Koukach <amirakoukach5066@gmail.com>
2023-12-16 10:29:17 +0200

    Added requests with query parameters and path parameters.

--- src/main/java/project2/controllers/SpringController.java
+++ src/main/java/project2/controllers/SpringController.java
@@ -2,11 +2,13 @@ package project2.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SpringController {
-    @GetMapping("hello")
+    @GetMapping("helloSpring")
     @ResponseBody
     public String hello() {
         return "Hello Spring!";
@@ -16,5 +18,19 @@ public class SpringController {
     @ResponseBody
     public String goodbye() {
         return "Goodbye Spring!";
+    }
+
+    // Handles request of the form /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello " + name + "!";
+    }
+
+    // Handles request of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
